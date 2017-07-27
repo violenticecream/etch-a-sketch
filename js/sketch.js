f19ab9ba4ed00de69183768b4348e44b7100daff
Chris Dyer <chris.dyer11@gmail.com>
2017-07-27 11:35:38 -0600

    Finally got JS functioning within set space. Not optimal

--- js/sketch.js
+++ js/sketch.js
@@ -1,24 +1,22 @@
-// Button to generate new divs
 
 
-$(document).ready(function() {
-	$("#createButton").click(function() {
-		genBoxes();
-	});	
 
-	$("#clearButton").click(function() {
-		clearScreen();
-	});
-});
-
-function genBoxes() {
-	var boxInt = 16;
+function genBoxes(boxInt) {
+	var boxSize = 800 / boxInt;
 	var boxNum = boxInt * boxInt;
+	var boxInt = 16;
 	var cont = [];
+	console.log(boxNum);
 	for (var i = 0; i < boxNum; i++) {
 		cont.push('<div class="box"></div>');
 		$('#boxWrap').html(cont.join(''));
 	};
+
+	$('.box').css({
+		height: boxSize + 'px',
+		width: boxSize + 'px'
+	});
+
 	$('.box').hover(function() {
 		$(this).addClass('boxHover');
 	}, function() {
@@ -29,13 +27,16 @@ function genBoxes() {
 
 function clearScreen() {
 	boxInt = prompt("How many boxes per side?", '16');
-	boxSize = 800 / boxInt;
-	cont = [];
-
-	$('.box').css({
-		height: boxSize + 'px',
-		width: boxSize + 'px'
-	});
-	
-	
+	genBoxes(boxInt);
 };
+
+$(document).ready(function() {
+	$("#createButton").click(function() {
+		genBoxes(boxInt);
+	});	
+
+	$("#clearButton").click(function() {
+		clearScreen();
+	});
+});
+
